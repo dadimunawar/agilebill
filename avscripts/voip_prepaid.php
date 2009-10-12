4af27f8cefc5e42fb025c19f06ed19fc5f17933f
sluther <sluther@bitpiston.com>
2009-10-11 22:54:10 -0700

    Updating voip_prepaid

--- avscripts/voip_prepaid.php
+++ avscripts/voip_prepaid.php
@@ -7,7 +7,8 @@
     $agi = new AGI();
     // answer the call
     $agi->answer();
-    // $agi->text2wav("This is a test");
-    $agi->say_digits(1234)
+    $agi->text2wav("Please enter your PIN and press the pound key.");
+    $result = $agi->get_data('beep', 3000, 20);
+    $keys = $result['result'];
+    $agi->text2wave("You entered $keys");
     
-?>
