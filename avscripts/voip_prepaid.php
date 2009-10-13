d435024504f47618bbe9b2e35d9f119e7cceb014
sluther <sluther@bitpiston.com>
2009-10-13 16:52:40 -0700

    Updating voip_prepaid.php script

--- avscripts/voip_prepaid.php
+++ avscripts/voip_prepaid.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php -q
 <?php
-
+    require('/var/www/html/agile/config.inc.php');
+    $dbh = new PDO('mysql:host='.AGILE_DB_HOST.';dbname='.AGILE_DB_DATABASE, AGILE_DB_USERNAME, AGILE_DB_PASSWORD);
     // require the file
     require('phpagi/phpagi.php');
     // instantiate the class
     $agi = new AGI();
     // answer the call
     $agi->answer();
-    $agi->text2wav("Please enter your PIN and press the pound key.");
-    $result = $agi->get_data('beep', 5000, 20);
+    //$agi->say_phonetic('Please enter your PIN followed by the pound key after the beep');
+
+    $result = $agi->get_data('beep', 30000, 11);
     $keys = $result['result'];
-    $agi->text2wav("You entered $keys");
+    
     $agi->hangup();
