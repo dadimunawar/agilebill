ed4ed4426b97ff28dbc5a9557478d87101f4722f
sluther <sluther@bitpiston.com>
2009-10-13 17:03:15 -0700

    Updating voip_prepaid.php script

--- avscripts/voip_prepaid.php
+++ avscripts/voip_prepaid.php
@@ -1,7 +1,12 @@
 #!/usr/bin/php -q
 <?php
     require('/var/www/html/agile/config.inc.php');
-    $dbh = new PDO('mysql:host='.AGILE_DB_HOST.';dbname='.AGILE_DB_DATABASE, AGILE_DB_USERNAME, AGILE_DB_PASSWORD);
+    try {
+        $dbh = new PDO('mysql:host='.AGILE_DB_HOST.';dbname='.AGILE_DB_DATABASE, AGILE_DB_USERNAME, AGILE_DB_PASSWORD);
+    } catch (PDOException $e) {
+        print 'Error!: ' . $e->getMessage() . "\n";
+        die();
+    }
     // require the file
     require('phpagi/phpagi.php');
     // instantiate the class
